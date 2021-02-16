a2fab97dd35032f08c1f49b3ab0481dff2d53e53
Spencer <james.spencer.thompson@gmail.com>
2021-02-16 10:32:26 -0600

    Water slows player

--- SpearSnail/Assets/Scripts/MoveWASD.cs
+++ SpearSnail/Assets/Scripts/MoveWASD.cs
@@ -8,26 +8,41 @@ public class MoveWASD : MonoBehaviour
     private float horizontal, vertical;
     private float moveLimiter = 0.8f;
     public float moveSpeed = 5f;
+    public float waterSlow = 0.2f;
+    private bool swimming;
+    public float moving;
+
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        swimming = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
+            horizontal = Input.GetAxisRaw("Horizontal");
+            vertical = Input.GetAxisRaw("Vertical");
     }
 
     private void FixedUpdate()
     {
+        moving = moveSpeed;
         if (horizontal != 0 && vertical != 0)
         {
             horizontal *= moveLimiter;
             vertical *= moveLimiter;
         }
-        body.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+        if (swimming)
+        {
+             moving = moving / waterSlow;
+        }
+        body.velocity = new Vector2(horizontal * moving, vertical * moving);
+    }
+
+    public void Swim(bool swimming)
+    {
+        this.swimming = swimming;
     }
 }
