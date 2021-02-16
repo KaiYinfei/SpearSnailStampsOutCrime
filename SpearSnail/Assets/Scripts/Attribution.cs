d3ed2e4bfaddd806355c4fc712edcf12c0f42818
Spencer <james.spencer.thompson@gmail.com>
2021-02-15 20:35:38 -0600

    Tilemap Added

---
+++ SpearSnail/Assets/Scripts/Attribution.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attribution : MonoBehaviour
+{
+    /*
+     * Tilemap Pixel Art from https://kvsr.itch.io/citypackpixelart
+    */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
