20471a23be72960f216dd9ad22ff1455d340b066
Spencer <james.spencer.thompson@gmail.com>
2021-02-15 18:28:08 -0600

    Janky Camera Follow and Snail Pixel made

---
+++ SpearSnail/Assets/Scripts/CameraFollows.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollows : MonoBehaviour
+{
+    public GameObject player;
+    public float boundaryPercent;
+    private float lBound, rBound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lBound = boundaryPercent * Camera.main.pixelWidth;
+        rBound = Camera.main.pixelWidth - lBound;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(player)
+        {
+            Vector3 spriteLoc = Camera.main.WorldToScreenPoint(player.transform.position);
+            Vector3 pos = transform.position;
+
+            if (spriteLoc.x < lBound)
+            {
+                pos.x -= lBound - spriteLoc.x;
+            }
+            else if (spriteLoc.x > rBound)
+            {
+                pos.x += spriteLoc.x - rBound;
+            }
+
+            transform.position = pos;
+        }
+    }
+}
